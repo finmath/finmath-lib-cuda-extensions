0dee8a93d285935def0c747dd7497e044a0a8cce
Christian Fries <email@christian-fries.de>
2019-08-26 16:55:15 +0200

    Set number of iterations to 25

--- src/test/java/net/finmath/montecarlo/interestrates/LIBORMarketModelCalibrationTest.java
+++ src/test/java/net/finmath/montecarlo/interestrates/LIBORMarketModelCalibrationTest.java
@@ -61,7 +61,7 @@ public class LIBORMarketModelCalibrationTest {
 
 	private final int numberOfPaths		= 100000;
 	private final int numberOfFactors	= 5;
-	private static final int maxIterations = 30;
+	private static final int maxIterations = 25;
 
 	private static final DecimalFormat formatterReal2		= new DecimalFormat(" 0.00");
 	private static final DecimalFormat formatterValue		= new DecimalFormat(" ##0.000%;-##0.000%", new DecimalFormatSymbols(Locale.ENGLISH));
