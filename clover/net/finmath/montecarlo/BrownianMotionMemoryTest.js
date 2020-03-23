1804dc437d55b1adb338b9059160186e925735df
Christian Fries <email@christian-fries.de>
2020-03-23 12:16:48 +0100

    Site checkin for project finmath-lib-cuda-extensions

---
+++ clover/net/finmath/montecarlo/BrownianMotionMemoryTest.js
@@ -0,0 +1,10 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = {"classes":[{"el":86,"id":3985,"methods":[{"el":43,"sc":2,"sl":33},{"el":85,"sc":2,"sl":45}],"name":"BrownianMotionMemoryTest","sl":26}]}
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {"test_14":{"methods":[{"sl":45}],"name":"testBrownianMotionMemory","pass":true,"statements":[{"sl":48},{"sl":49},{"sl":50},{"sl":53},{"sl":55},{"sl":57},{"sl":58},{"sl":59},{"sl":60},{"sl":61},{"sl":62},{"sl":63},{"sl":64},{"sl":67},{"sl":74},{"sl":75},{"sl":76},{"sl":78},{"sl":79},{"sl":82},{"sl":84}]}}
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [14], [], [], [14], [14], [14], [], [], [14], [], [14], [], [14], [14], [14], [14], [14], [14], [14], [14], [], [], [14], [], [], [], [], [], [], [14], [14], [14], [], [14], [14], [], [], [14], [], [14], [], []]
---
+++ clover/package-nodes-tree.js
@@ -0,0 +1,143 @@
+ 
+
+var Packages = {
+    nodes: [
+                                                                                                        
+                
+{
+    "id": "net.finmath",
+    "text": "net.finmath",
+    "package": "",
+    "url": "",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+        "a_attr": {"href": ""},
+    "children": [
+                                            
+                
+{
+    "id": "net.finmath.jcuda",
+    "text": "jcuda",
+    "package": "net.finmath.jcuda",
+    "url": "net/finmath/jcuda/pkg-summary.html",
+            "coverage": "69.2%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "net/finmath/jcuda/pkg-summary.html"},
+    "children": [
+                    ]
+},
+                                    
+                
+{
+    "id": "net.finmath.montecarlo",
+    "text": "montecarlo",
+    "package": "net.finmath.montecarlo",
+    "url": "net/finmath/montecarlo/testsrc-pkg-summary.html",
+            "coverage": "99.6%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "net/finmath/montecarlo/testsrc-pkg-summary.html"},
+    "children": [
+                                            
+                
+{
+    "id": "net.finmath.montecarlo.assetderivativevaluation",
+    "text": "assetderivativevaluation",
+    "package": "net.finmath.montecarlo.assetderivativevaluation",
+    "url": "net/finmath/montecarlo/assetderivativevaluation/testsrc-pkg-summary.html",
+            "coverage": "82.1%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "net/finmath/montecarlo/assetderivativevaluation/testsrc-pkg-summary.html"},
+    "children": [
+                    ]
+},
+                                    
+                
+{
+    "id": "net.finmath.montecarlo.cpu",
+    "text": "cpu",
+    "package": "net.finmath.montecarlo.cpu",
+    "url": "net/finmath/montecarlo/cpu/pkg-summary.html",
+            "coverage": "51.4%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "net/finmath/montecarlo/cpu/pkg-summary.html"},
+    "children": [
+                    ]
+},
+                                    
+                
+{
+    "id": "net.finmath.montecarlo.cuda",
+    "text": "cuda",
+    "package": "net.finmath.montecarlo.cuda",
+    "url": "net/finmath/montecarlo/cuda/pkg-summary.html",
+            "coverage": "64.7%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "net/finmath/montecarlo/cuda/pkg-summary.html"},
+    "children": [
+                                            
+                
+{
+    "id": "net.finmath.montecarlo.cuda.alternative",
+    "text": "alternative",
+    "package": "net.finmath.montecarlo.cuda.alternative",
+    "url": "net/finmath/montecarlo/cuda/alternative/pkg-summary.html",
+            "coverage": "15.4%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "net/finmath/montecarlo/cuda/alternative/pkg-summary.html"},
+    "children": [
+                    ]
+},
+            ]
+},
+                                    
+                
+{
+    "id": "net.finmath.montecarlo.interestrates",
+    "text": "interestrates",
+    "package": "net.finmath.montecarlo.interestrates",
+    "url": "net/finmath/montecarlo/interestrates/testsrc-pkg-summary.html",
+            "coverage": "99.2%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "net/finmath/montecarlo/interestrates/testsrc-pkg-summary.html"},
+    "children": [
+                    ]
+},
+                                    
+                
+{
+    "id": "net.finmath.montecarlo.opencl",
+    "text": "opencl",
+    "package": "net.finmath.montecarlo.opencl",
+    "url": "net/finmath/montecarlo/opencl/pkg-summary.html",
+            "coverage": "63.6%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "net/finmath/montecarlo/opencl/pkg-summary.html"},
+    "children": [
+                    ]
+},
+            ]
+},
+            ]
+},
+            ],
+    settings: {
+        "icons": {
+            "package": {
+                "open": "aui-icon aui-icon-small aui-iconfont-devtools-folder-open",
+                "closed": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed"
+            },
+            "state": {
+                "collapsed": "aui-icon aui-icon-small aui-iconfont-collapsed",
+                "expanded": "aui-icon aui-icon-small aui-iconfont-expanded",
+                "forRemoval": "hidden aui-iconfont-collapsed aui-iconfont-expanded"
+            }
+        }
+    }
+};
